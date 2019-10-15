586198dd90e26463c5df4c4189d6262aa0b775e6
Ryan Overton <ryan.overton@outlook.com>
2019-10-15 12:08:50 -0500

    Add live coders url command (#5)

---
+++ ChatCommands/LiveCodersUrlCommand.cs
@@ -0,0 +1,25 @@
+﻿using Service.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatCommands
+{
+    public class LiveCodersUrlCommand : IChatCommand
+    {
+        private const string liveCodersUrl = "https://twitch.tv/team/livecoders";
+        public string Command => "livecoders";
+
+        public string Description => "Displays Twitch URL for Live Coders team";
+
+        public TimeSpan? Cooldown => TimeSpan.FromSeconds(10);
+
+        public bool CanBeListed() => true;
+
+        public async Task Execute(IChatService service, bool isBroadcaster, string userName, ReadOnlyMemory<char> text)
+        {
+            await service.SendMessage($"Check out all the Live Coders at {liveCodersUrl}");
+        }
+    }
+}
